666f74f081ed5bc53c8dff9d7bce51e40d1b5710
Quinton Woodmansee <qwoodmansee@capital.edu>
2016-10-11 21:56:32 -0400

    Began Implementation of smart weights, including python script to pull data

--- imports/helpers/WeightGenerator.js
+++ imports/helpers/WeightGenerator.js
@@ -8,7 +8,7 @@ export const WeightGenerator = function() {
     self.generateEqualWeights = function() {
         var weights = {};
         for (var i=0; i < self.allItems.length; i++) {
-            weights[self.allItems[i]] = 1;
+            weights[self.allItems[i][0]] = 1;
         }
         return weights;
     };
@@ -22,9 +22,31 @@ export const WeightGenerator = function() {
             } else {
                 temp = self.generateRandomNumber(1,5, false)
             }
-            weights[self.allItems[i]] = temp;
+            weights[self.allItems[i][0]] = temp;
         }
         return weights;
+    };
+
+    self.generateSmartWeights = function(allowNegative, RBA) {
+        var weights = {};
+        var mean;
+        var stdev;
+        //for each item
+        for (var i=0; i < self.allItems.length; i++) {
+            //get it's mean and stdev
+            if (RBA){
+                mean = parseInt(self.allItems[i][1]);
+                stdev = parseInt(self.allItems[i][2]);
+            } else {
+                mean = parseInt(self.allItems[i][4]);
+                stdev = parseInt(self.allItems[i][5]);
+            }
+            var standard = self._gaussian(mean, stdev);
+            temp = Math.floor(standard());
+            weights[self.allItems[i][0]] = temp;
+        }
+
+        return weights
     };
 
     self.generateRandomNumber = function(positiveMin, max, allowNegative) {
@@ -39,5 +61,37 @@ export const WeightGenerator = function() {
         }
         return num
     };
+
+    //helper function which will allow a standard distribution to be created to find weights based on mean and std dev
+    //src: http://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
+    self._gaussian = function(mean, stdev) {
+        var y2;
+        var use_last = false;
+        return function() {
+            var y1;
+            if(use_last) {
+                y1 = y2;
+                use_last = false;
+            }
+            else {
+                var x1, x2, w;
+                do {
+                    x1 = 2.0 * Math.random() - 1.0;
+                    x2 = 2.0 * Math.random() - 1.0;
+                    w  = x1 * x1 + x2 * x2;
+                } while( w >= 1.0);
+                w = Math.sqrt((-2.0 * Math.log(w))/w);
+                y1 = x1 * w;
+                y2 = x2 * w;
+                use_last = true;
+            }
+
+            var retval = mean + stdev * y1;
+            if(retval > 0)
+                return retval;
+            return -retval;
+        }
+    }
+
 
 };
--- scripts/getOoTBingoData/main.py
+++ scripts/getOoTBingoData/main.py
@@ -4,32 +4,59 @@ import json
 
 def main():
 
-    name = 'name'
-    difficulty = 'difficulty'
-    time = 'time'
-    skill = 'skill'
-    timey = '#timey'
-    outputFileName = 'raceData.json'
+    validOption = False
 
-    url = "https://docs.google.com/spreadsheets/d/1dRpwfIV2vDRL_Hq-pBj3U7wq7XwZ9JPW9Ac8hK5qbgc/export?format=csv&id=1dRpwfIV2vDRL_Hq-pBj3U7wq7XwZ9JPW9Ac8hK5qbgc&gid=0"
-    response = urllib2.urlopen(url)
-    cr = csv.reader(response)
+    while not validOption:
+        choice = raw_input("Enter 1 for Bingo Data or 2 for Item Weight Data: \n")
+        if eval(choice) == 1 or eval(choice) == 2:
+            validOption = True
 
-    i = 0
-    dict1 = {}
+    if eval(choice) == 1:
+        name = 'name'
+        difficulty = 'difficulty'
+        time = 'time'
+        skill = 'skill'
+        timey = '#timey'
+        outputFileName = 'raceData.json'
 
-    for row in cr:
-        if i != 0 and i != 1 and i < 193 and row[1] != "":
-            dict2 = {}
-            dict2[name] = row[1]
-            dict2[difficulty] = row[3]
-            dict2[time] = row[4]
-            dict2[skill] = row[5]
-            dict2[timey] = row[6]
-            dict1[row[1]] = dict2
-        i += 1
+        url = "https://docs.google.com/spreadsheets/d/1dRpwfIV2vDRL_Hq-pBj3U7wq7XwZ9JPW9Ac8hK5qbgc/export?format=csv&id=1dRpwfIV2vDRL_Hq-pBj3U7wq7XwZ9JPW9Ac8hK5qbgc&gid=0"
+        response = urllib2.urlopen(url)
+        cr = csv.reader(response)
 
-    with open(outputFileName, 'w') as outfile:
-        json.dump(dict1, outfile, indent=4)
+        i = 0
+        dict1 = {}
 
+        for row in cr:
+            if i != 0 and i != 1 and i < 193 and row[1] != "":
+                dict2 = {}
+                dict2[name] = row[1]
+                dict2[difficulty] = row[3]
+                dict2[time] = row[4]
+                dict2[skill] = row[5]
+                dict2[timey] = row[6]
+                dict1[row[1]] = dict2
+            i += 1
+
+        with open(outputFileName, 'w') as outfile:
+            json.dump(dict1, outfile, indent=4)
+    else:
+        url = 'https://docs.google.com/spreadsheets/u/0/d/1ry0cdJ4MrImkCsfLHvWAKD9Ude_W3641UnJGBQ2zseU/export?format=csv&id=1ry0cdJ4MrImkCsfLHvWAKD9Ude_W3641UnJGBQ2zseU&gid=0'
+        name = 'name'
+        mean = 'mean'
+        stDev = 'Std. Dev'
+        negChance = 'Negative Chance'
+        outputFileName = 'itemInfo.js'
+
+        response = urllib2.urlopen(url)
+        cr = csv.reader(response)
+
+        i = 0
+        finalArray = []
+        for row in cr:
+            if i != 0:
+                finalArray.append(row)
+            i += 1
+
+        with open(outputFileName, 'w') as outfile:
+            json.dump(finalArray, outfile, indent=1)
 main()
