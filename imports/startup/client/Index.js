814947321bac72a4559d60f77c897ffca5ee055a
Quinton Woodmansee <qwoodmansee@capital.edu>
2020-06-10 22:35:15 -0400

    bugfix on twitch viewer part 1

--- imports/startup/client/Index.js
+++ imports/startup/client/Index.js
@@ -1,4 +1,12 @@
 /**
  * Created by Quinton on 9/24/2016.
  */
+import { WebApp } from 'meteor/webapp';
 import './Routes.js';
+
+Meteor.startup(() => {
+  WebApp.rawConnectHandlers.use((req, res, next) => {
+  res.setHeader('X-Frame-Options', 'ALLOW-FROM https://twitch.tv');
+  return next();
+  });
+})
